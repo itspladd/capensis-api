6da669d655606c97dc816b6da025bc636d5e5e78
Paul Ladd <vlendle@gmail.com>
2021-09-05 13:44:06 -0500

    Add seed files with basic seeds for projects. Edit blocks schema to have start/end time

--- src/db/schema/03_blocks.sql
+++ src/db/schema/03_blocks.sql
@@ -4,5 +4,6 @@ CREATE TABLE blocks (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   project_id INTEGER REFERENCES projects(id),
-  hashed_password VARCHAR(255)
+  start_time TIMESTAMP NOT NULL,
+  end_time TIMESTAMP NOT NULL
 );
---
+++ src/db/seeds/02_projects.sql
@@ -0,0 +1,7 @@
+INSERT INTO projects (user_id, title)
+VALUES
+(1, 'PHP'),
+(1, 'React'),
+(2, 'Demotic'),
+(2, 'Middle Egyptian')
+;
