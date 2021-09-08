5249dc41fefc28bc6fe3f14799b6eccd22c50267
Paul Ladd <vlendle@gmail.com>
2021-09-07 20:27:00 -0500

    Update report to return total time tracked/blocked in seconds

--- src/db/dbHelpers.js
+++ src/db/dbHelpers.js
@@ -60,7 +60,7 @@ module.exports = function (db) {
         JOIN projects
         ON blocks.project_id = projects.id
       WHERE blocks.user_id = $1
-      AND schedule_date BETWEEN $2 AND $3
+      AND start_time BETWEEN $2 AND $3
       ORDER BY start_time
       `, [userId, lastSunday.toISOString(), nextSaturday.toISOString()])
   }
@@ -114,19 +114,28 @@ module.exports = function (db) {
       `, [userId, lastSunday.toISOString()]);
   }
 
+  // getWeeklyReport finds every project that:
+  // 1. Belongs to this user
+  // 2. Has blocks scheduled this week
+  //    (note that we don't care if any sessions are logged for this project or not)
+  // Then sum up the time deltas for the sessions and blocks to get the total time blocked and total time tracked
+  // If no time was tracked, "sessions_total" will be null.
+  // NOTE: "EXTRACT EPOCH" is used to turn the time interval into a number of seconds.
+  // That way we can calculate hours on the frontend.
   const getWeeklyReport = (userId, targetDate = new Date()) => {
     const { lastSunday, nextSaturday } = getWeekBounds(targetDate);
+
     return db.query(`
       SELECT
         projects.id AS project_id,
-        SUM(sessions.end_time-sessions.start_time) AS sessions_total,
-        SUM(blocks.end_time-blocks.start_time) AS blocks_total
+        SUM(EXTRACT(EPOCH FROM (sessions.end_time-sessions.start_time))) AS sessions_total,
+        SUM(EXTRACT(EPOCH FROM (blocks.end_time-blocks.start_time))) AS blocks_total
       FROM projects
         LEFT JOIN sessions ON projects.id = sessions.project_id
         JOIN blocks ON projects.id = blocks.project_id
       WHERE projects.user_id = $1
-        AND (sessions.id IS NULL OR (sessions.start_time > $2 AND sessions.end_time < $3))
-        AND (blocks.start_time > $2 AND blocks.end_time < $3)
+        AND (sessions.id IS NULL OR (sessions.start_time BETWEEN $2 AND $3))
+        AND (blocks.start_time BETWEEN $2 AND $3)
       GROUP BY projects.id
       `, [userId, lastSunday.toISOString(), nextSaturday.toISOString()]);
   }
