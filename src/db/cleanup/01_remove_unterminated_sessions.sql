73e4c0fe722c8e3dd735a22b2cc05e13d4fe5e66
Paul Ladd <vlendle@gmail.com>
2021-10-11 15:02:38 -0500

    Update cleanup 01 with documentation since it will probably confuse me later

--- src/db/cleanup/01_remove_unterminated_sessions.sql
+++ src/db/cleanup/01_remove_unterminated_sessions.sql
@@ -1,3 +1,8 @@
+/* Remove all unterminated sessions (i.e. end_time is null)
+   EXCEPT for the most recent session of each user
+   (so that we don't remove any sessions that are
+   supposed to still be running)
+ */
 DELETE FROM sessions AS big_s
 WHERE (id < (
   SELECT MAX(s.id) FROM sessions AS s
